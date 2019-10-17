b7b8e3e7e0eea0b244c39ead2c4ac8810d050604
n e <d4ngertim3@gmail.com>
2019-10-17 21:23:00 +0300

    competitive programming template

---
+++ codes/tpl.cpp
@@ -0,0 +1,20 @@
+#include<cstring>
+#include<cassert>
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<map>
+#include<string>
+using namespace std;
+using ll=long long;
+
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    return 0;
+}
